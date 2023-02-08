a94025f174e479bae8e6410f635713c5fe815113
Karyn Huston <huston.karyn@gmail.com>
2023-02-08 13:46:14 -0500

    delete a question

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -2,35 +2,37 @@ const express = require("express");
 const questions = express.Router();
 
 const {
-getAllQuestions, getOneQuestion, createAQuestion, updateAQuestion
-} = require("../queries/questions")
+  getAllQuestions,
+  getOneQuestion,
+  createAquestion,
+  updateAquestion,
+  deleteAquestion,
+} = require("../queries/questions");
 
-
-const {checkBody} = require("../validations/checkQuestions.js")
+const { checkBody } = require("../validations/checkQuestions.js");
 // INDEX
 questions.get("/", async (req, res) => {
-    const all_questions = await getAllQuestions();
-    if (all_questions[0]) {
-      res.status(200).json(all_questions);
-    } else {
-      res.status(500).json({ error: "server error" });
-    }
-
-  });
+  const all_questions = await getAllQuestions();
+  if (all_questions[0]) {
+    res.status(200).json(all_questions);
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
+});
 
 questions.get("/:question_id", async (req, res) => {
-    const {question_id} = req.params;
-    const one_question = await getOneQuestion(question_id);
-    if(one_question) {
-        res.status(200).json(one_question);
-    }else {
-        res.status(404).json({error: `bad request for ${question_id}` })
-    }
-})
+  const { question_id } = req.params;
+  const one_question = await getOneQuestion(question_id);
+  if (one_question) {
+    res.status(200).json(one_question);
+  } else {
+    res.status(404).json({ error: `bad request for question of id: ${question_id}` });
+  }
+});
 
 questions.post("/", async (req, res) => {
   try {
-    const ask_a_question = await createAQuestion(req.body);
+    const ask_a_question = await createAquestion(req.body);
     res.json(ask_a_question);
   } catch (error) {
     res.status(400).json({ error: "error" });
@@ -38,16 +40,23 @@ questions.post("/", async (req, res) => {
 });
 
 questions.put("/:question_id", async (req, res) => {
-  const {question_id} = req.params;
+  const { question_id } = req.params;
   try {
-  const update_question = await updateAQuestion(question_id, req.body);
-  res.status(200).json(update_question)
-  }catch (error) {
-    res.status(404).json({error: "error updating this question"})
+    const update_question = await updateAquestion(question_id, req.body);
+    res.status(200).json(update_question);
+  } catch (error) {
+    res.status(404).json({ error: "error updating this question" });
   }
+});
 
-
-})
-
+questions.delete("/:question_id", async (req, res) => {
+  const { question_id } = req.params;
+  const deleted_question = await deleteAquestion(question_id);
+  if (deleted_question.question_id) {
+    res.status(200).json(deleted_question);
+  } else {
+    res.status(404).json({ error: "question not found" });
+  }
+});
 
 module.exports = questions;
--- queries/questions.js
+++ queries/questions.js
@@ -20,25 +20,36 @@ const getOneQuestion = async (id) => {
 };
 
 
-const createAQuestion = async (question) => {
+const createAquestion = async (question) => {
   try {
-    const askAQuestion = await db.one("INSERT INTO questions (body, name, todays_date, topic, child_age) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
+    const askAquestion = await db.one("INSERT INTO questions (body, name, todays_date, topic, child_age) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
     [question.body, question.name, question.todays_date, question.topic, question.child_age ])
-    return askAQuestion;
+    return askAquestion;
   }catch (error) {
     return error;
   }
- }
+ };
 
 
- const updateAQuestion = async (question_id, question) => {
+ const updateAquestion = async (id, question) => {
   try {
-    const updatedQuestion = await db.one("UPDATE questions SET body=$1, name=$2, todays_date=$3, topic=$4, child_age=$5 WHERE id=$6 RETURNING *", 
-    [question.body, question.name, question.todays_date, question.topic, question.child_age, question_id]);
+    const updatedQuestion = await db.one("UPDATE questions SET body = $1, name = $2, todays_date = $3, topic = $4, child_age = $5 WHERE id = $6 RETURNING *", 
+    [question.body, question.name, question.todays_date, question.topic, question.child_age, id]);
     return updatedQuestion;
   }catch (error) {
     return error;
   }
+ };
 
- }
-module.exports = { getAllQuestions, getOneQuestion, createAQuestion, updateAQuestion };
+ const deleteAquestion = async (id) => {
+  try {
+    const deletedQuestion = await db.one("DELETE FROM questions WHERE id=$1 RETURNING *", id)
+    return deletedQuestion;
+  }catch (error) {
+    return error;
+  }
+ };
+
+
+ 
+module.exports = { getAllQuestions, getOneQuestion, createAquestion, updateAquestion, deleteAquestion };
