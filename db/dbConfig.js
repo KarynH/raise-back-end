372b21840e91d7dd0096d006704f857eeb2d2760
Karyn Huston <huston.karyn@gmail.com>
2023-02-09 01:00:27 -0500

    removed database url

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -5,8 +5,7 @@ const cn = {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-    DATABASE_URL : "postgres://raise_user:nR8d6O6YdL6hQvoyE0t7T9df48bSuJ5A@dpg-cfi7mo1gp3jh03i4kpr0-a/raise"
+    user: process.env.PG_USER
   };
 
   const db = pgp(cn);
