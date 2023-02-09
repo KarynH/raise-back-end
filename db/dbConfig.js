758645f5d5810ba16d0dd5c091d07b0cf439a202
Karyn Huston <huston.karyn@gmail.com>
2023-02-09 00:54:57 -0500

    database

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -6,6 +6,7 @@ const cn = {
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
+    DATABASE_URL : "postgres://raise_user:nR8d6O6YdL6hQvoyE0t7T9df48bSuJ5A@dpg-cfi7mo1gp3jh03i4kpr0-a/raise"
   };
 
   const db = pgp(cn);
