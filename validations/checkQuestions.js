e7da9d19b0fdf6c6f3ea8b79127ef8f11fbc7ec5
Karyn Huston <huston.karyn@gmail.com>
2023-02-08 21:36:47 -0500

    validation for empty  text body

--- validations/checkQuestions.js
+++ validations/checkQuestions.js
@@ -2,24 +2,25 @@ const checkQuestionBody = (req, res, next) => {
   const { body } = req.body;
   console.log("checking body");
   console.log(`BODY:${body}`);
-  if (body !== "" || body !== " " || body !== undefined) {
-    next();
-  } else {
-    res.status(400).json({ error: "A question body is required" });
-  }
-};
-
-const checkName = (req, res, next) => {
-  const { name } = req.body;
-  console.log("checking name ");
-  if (name !== "" || name !== " " || name !== undefined) {
-    console.log(`NAME ${name}`);
-    next();
-  } else {
-    res.status(400).json({ error: "A name is required" });
-  }
+ if(body != " ") {
+  next();
+ }else {
+  res.status(500).json({error: "a body for a question is required"})
+ }
 };
 
 
+// const checkName = (req, res, next) => {
+//   const { name } = req.body;
+//   console.log("checking name ");
+//   if (name !== "" || name !== " " || name !== undefined) {
+//     console.log(`NAME ${name}`);
+//     next();
+//   } else {
+//     res.status(400).json({ error: "A name is required" });
+//   }
+// };
 
-module.exports = { checkQuestionBody, checkName };
+
+
+module.exports = { checkQuestionBody };
