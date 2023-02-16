7def97187ebb5153c0328c52b9fe7b62e2bacad6
Karyn Huston <huston.karyn@gmail.com>
2023-02-15 23:48:00 -0500

    missing child age for answer queries

--- queries/answers.js
+++ queries/answers.js
@@ -5,7 +5,7 @@ const getAllAnswers = async (questionId) => {
   try {
     const allAnswers = await db.any(
       // "SELECT * FROM answers WHERE question_id=$1",
-      "SELECT answers.todays_date,answers.response,answers.provider_type,answers.id,answers.question_id,questions.body FROM answers JOIN questions ON answers.question_id=questions.id WHERE questions.id=$1",
+      "SELECT answers.todays_date,answers.response,answers.provider_type,answers.id,answers.question_id,question.child_age,questions.body FROM answers JOIN questions ON answers.question_id=questions.id WHERE questions.id=$1",
       [questionId]
     );
     return allAnswers;
