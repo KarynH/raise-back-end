a0b6e21350cd165151e37c7955893f5a35143b94
Karyn Huston <huston.karyn@gmail.com>
2023-02-18 19:11:43 -0500

    add else/if for question that doesnt have an answer

--- controllers/answersController.js
+++ controllers/answersController.js
@@ -5,11 +5,14 @@ const { getAllAnswers } = require("../queries/answers");
 
 answers.get("/", async (req, res) => {
   const { id } = req.params;
+  const {body, name, todays_date, child_age} = req.body;
   const all_answers = await getAllAnswers(id);
   if (all_answers[0]) {
     res.status(200).json(all_answers);
-  } else {
-    res.status(500).json({ error: "server error" });
+  }else if(id) {
+    res.status(200).json({body, name, todays_date, child_age})
+  }else {
+    res.status(200).json({ error: "server errorr" });
   }
 });
 
--- queries/answers.js
+++ queries/answers.js
@@ -14,4 +14,6 @@ const getAllAnswers = async (questionId) => {
   }
 };
 
+
+
 module.exports = { getAllAnswers };
